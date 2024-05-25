3afd1a1e68e2b80386174fc3ff43d3ffb3a8c6e6
MaLikTTCCCC <maliktrc@outlook.com>
2024-05-25 11:11:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import streamlit as sl
-sl.write("""fucking Hell""")
+import streamlit as st
+st.write("""fucking Hell""")
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(
-     [1,2,4],
-     columns=['a', 'b', 'c'])
 
-sl.line_chart(chart_data)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
