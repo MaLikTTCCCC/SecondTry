d27495c95a2e71431815928e53288e58009bd7cf
MaLikTTCCCC <maliktrc@outlook.com>
2024-05-25 11:05:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlib as sl
 sl.write("""fucking Hell""")
+import numpy as np
+import pandas as pd
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+sl.line_chart(chart_data)
