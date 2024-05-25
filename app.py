928b42a791cd7107d5edb889e3a1e5d567336d81
MaLikTTCCCC <maliktrc@outlook.com>
2024-05-25 11:45:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import yfinance as yf
 
 #st.map(map_data)
 
-msft = yf.download("msft", start="2020-01-01", end="2021-01-01")
+msft = yf.download("msft", start="2020-01-01", end="2020-05-05")
 aapl=yf.download("aapl", start="2020-01-01", end="2021-01-01")
 meta=yf.download("meta", start="2020-01-01", end="2021-01-01")
 amzn=yf.download("amzn", start="2020-01-01", end="2021-01-01")
@@ -19,5 +19,5 @@ nflx=yf.download("nflx", start="2020-01-01", end="2021-01-01")
 
 #line=st.line_chart(Rate[0:1])
 #msft['Close'].plot()
-st.pyplot(aapl.plot.barh(stacked=True).figure)
+st.pyplot(msft.plot.barh(stacked=True).figure)
 
